4bf42de2128b1eae7ee86a40d0930d798764cbc3
Wojciech Kawa <k4wu5@protonmail.com>
2024-01-07 21:07:10 +0100

    feat: create a layout system and fill it with basic information

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header>
+      <h1>header</h1>
+    </header>
+
+    <main>
+      <slot />
+    </main>
+
+    <footer>
+      <h1>footer</h1>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ layouts/task-layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header>
+      <h1>header</h1>
+    </header>
+
+    <main>
+      <p>TaskApp Nuxt</p>
+      <slot />
+    </main>
+
+    <footer>
+      <h1>footer</h1>
+    </footer>
+  </div>
+</template>
